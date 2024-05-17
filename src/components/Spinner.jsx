ac5acc86477f239a0407e91495b36b10b50f98e2
emmanuelrichard01 <emma.moghalu@gmail.com>
2024-05-17 22:05:38 +0100

    Initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,18 @@
+import GridLoader from 'react-spinners/GridLoader';
+
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+const Spinner = ({ loading }) => {
+  return (
+    <GridLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={20}
+    />
+  );
+};
+
+export default Spinner;
